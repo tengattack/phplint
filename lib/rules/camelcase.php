53d0953d6f0cf1b8c0d30cde10fbdc9fe2835efe
tengattack <tengattack@foxmail.com>
2017-12-12 15:10:10 +0800

    Update rule camelcase options

--- lib/rules/camelcase.php
+++ lib/rules/camelcase.php
@@ -2,7 +2,28 @@
 
 use Microsoft\PhpParser\TokenKind;
 
-class NoCamelCaseRule extends Rule {
+class CamelCaseRule extends Rule {
+
+  private $camelCase = true;
+  private $ignore = [];
+
+  protected function makeOptions() {
+    if (!empty($this->options)) {
+      $opt = $this->options[0];
+      if (is_string($opt)) {
+          $this->camelCase = $opt === 'always';
+          if (count($this->options) > 1) {
+            $opt = $this->options[1];
+          }
+      }
+      if (is_array($opt)) {
+        if (array_key_exists('ignore', $opt)) {
+          $this->ignore = $opt['ignore'];
+        }
+      }
+    }
+  }
+
   public function filters() {
     return [
       'Variable',
@@ -10,12 +31,12 @@ class NoCamelCaseRule extends Rule {
     ];
   }
 
+  private function isUnderscored($name) {
+    // if there's an underscore, it might be A_CONSTANT, which is okay
+    return strpos($name, '_') !== false && $name !== strtoupper($name);
+  }
+
   private function checkCamelCase(string $name) {
-    /*
-     * Strip leading and trailing underscores as there are commonly used to flag
-     * private/protected identifiers
-     */
-    $name = preg_replace('/^_+|_+$/', '', $name);
     // it might be A_CONSTANT, which is okay
     return $name !== strtolower($name)
       && $name !== strtoupper($name)
@@ -29,7 +50,21 @@ class NoCamelCaseRule extends Rule {
         if ($name && $name[0] === '$') {
           $name = substr($name, 1);
         }
-        if ($this->checkCamelCase($name)) {
+        if (in_array($name, $this->ignore)) {
+          return;
+        }
+        /**
+         * Strip leading and trailing underscores as there are commonly used to flag
+         * private/protected identifiers
+         */
+        $name = preg_replace('/^_+|_+$/', '', $name);
+        if ($this->camelCase) {
+          // not allow underscore
+          if ($this->isUnderscored($name)) {
+            $this->report($node, "Identifier '$name' is not in camel case.");
+          }
+        } elseif ($this->checkCamelCase($name)) {
+          // not allow camelcase
           $this->report($node, "Identifier '$name' is in camel case.");
         }
       }
@@ -45,4 +80,4 @@ class NoCamelCaseRule extends Rule {
   }
 }
 
-return 'NoCamelCaseRule';
+return 'CamelCaseRule';
