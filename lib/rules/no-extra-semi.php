e3365dec779564066441002a105f70c866f600a9
tengattack <tengattack@foxmail.com>
2017-12-21 22:39:09 +0800

    Update rule no-extra-semi check for case statement semicolon

--- lib/rules/no-extra-semi.php
+++ lib/rules/no-extra-semi.php
@@ -1,15 +1,31 @@
 <?php
 
+use Microsoft\PhpParser\TokenKind;
+
 class NoExtraSemiRule extends Rule {
 
   public function filters() {
     return [
       'EmptyStatement',
+      'CaseStatementNode',
     ];
   }
 
   public function EmptyStatement(&$node) {
     $this->report($node, 'Unnecessary semicolon.');
+  }
+
+  public function CaseStatementNode(&$node) {
+    $idx = 0;
+    foreach ($node->getChildTokens() as $token) {
+      $idx++;
+      if ($idx >= 2) {
+        if ($token->kind === TokenKind::SemicolonToken) {
+          $this->report($node, $token->start, "The colon token ':' is preferrable for case statement.");
+        }
+        break;
+      }
+    }
   }
 
 }
