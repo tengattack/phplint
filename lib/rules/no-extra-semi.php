e44bcc28c847e669875089b649660ec7a62c35ea
tengattack <tengattack@foxmail.com>
2018-04-26 12:05:43 +0800

    Update rule no-extra-semi check short open tag semi

--- lib/rules/no-extra-semi.php
+++ lib/rules/no-extra-semi.php
@@ -4,10 +4,15 @@ use Microsoft\PhpParser\TokenKind;
 
 class NoExtraSemiRule extends Rule {
 
+  private $shortOpenTag = false;
+
   public function filters() {
     return [
       'EmptyStatement',
       'CaseStatementNode',
+      'ExpressionStatement',
+      'ScriptSectionStartTag',
+      'ScriptSectionEndTag',
     ];
   }
 
@@ -26,6 +31,29 @@ class NoExtraSemiRule extends Rule {
         break;
       }
     }
+  }
+
+  public function ExpressionStatement(&$node) {
+    if ($this->shortOpenTag) {
+      $parent = $node->getParent();
+      if (!$parent || $parent->getNodeKindName() === 'SourceFileNode') {
+        // check whether the root node
+        foreach ($node->getChildTokens() as $token) {
+          if ($token->kind === TokenKind::SemicolonToken) {
+            $this->report($node, $token->start, 'Unnecessary semicolon.');
+          }
+          break;
+        }
+      }
+    }
+  }
+
+  public function ScriptSectionStartTag(&$token) {
+    $this->shortOpenTag = $this->context->sourceCode->getTokenText($token) === '<?=';
+  }
+
+  public function ScriptSectionEndTag(&$token) {
+    $this->shortOpenTag = false;
   }
 
 }
