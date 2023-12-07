060ac3fea08383d6c187658794e2fb367454b0ad
fish <353740902@qq.com>
2023-12-07 14:31:44 +0800

    添加 REVIEW

--- tests/rules/spaced-comment.php
+++ tests/rules/spaced-comment.php
@@ -13,14 +13,16 @@ final class SpacedCommentRuleTest extends RuleTestCase {
 // FIXME：xxx
 // WORKAROUND：xxx
 // NOTICE：xxx
+// REVIEW：xxx
 // TEMP: xxx
 // TODO: xxx
 // FIXME: xxx
 // WORKAROUND: xxx
 // NOTICE: xxx
+// REVIEW: xxx
 EOF;
         $rules = ['spaced-comment' => ['error', 'always', ['exceptions' => '-+*']]];
         $report = processSource($source, $rules);
-        $this->assertLineColumn([[3, 1], [4, 1], [5, 1], [6, 1], [7, 1]], $report);
+        $this->assertLineColumn([[3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1]], $report);
     }
 }
